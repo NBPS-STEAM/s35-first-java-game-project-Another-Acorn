1bb6ce2e4265e05145c66f58584378e8fdbcc514
AnotherAcorn <gnarlyseacucumber@gmail.com>
2021-01-06 23:22:44 +0000

    fixed bugs and worked on adding a "win"

--- App.java
+++ App.java
@@ -12,42 +12,47 @@ public class App {
      static String B2 = "_";
      static String B3 = "_";
      static String C1 = "_";
-     static String C2 = "_";
+     static String C2 = "_"; 
      static String C3 = "_";
-     static String playAgain = true;
+     static boolean noWinner = false;
      static boolean win1 = false;
-     static Scanner input = new Scanner(System.in);
+     
 
     public static void main(String[] args) {
-      
-//start while loop and game 
-      while (playAgain = true);
-      {
+      Scanner input = new Scanner(System.in);
 //print intro
         System.out.println("Let's play a game of tic-tac-toe" + "\nPick a slot to put your piece in using \nA1, A2, A3, B1, B2, B3, C1, C2, or C3" + "\nYou can win by claiming all A slots, all 1 slots, or A1, B2, C3 or A3, B2, or C1");
+        noWinner = true;
 //create users and their names
         System.out.println("Enter player 1's name:");
         p1 = input.nextLine();
         System.out.println("Enter player 2's name:");
         p2 = input.nextLine();
         System.out.println("Player 1: " + p1 + "\nPlayer 2: " + p2);
+//start while loop and game 
+      while (noWinner = true)
+      {
 //player 1 plays
         System.out.println("Give " + p1 + "'s next move:");
         move1 = input.nextLine();
         moveItt(move1);
         printBoard();
+//Checks for Player 1 having won
+        Winner1(win1, A1, A2, A3, B1, B2, B3,  C1, C2, C3);
+        if (win1 == true)
+        {
+          System.out.println(p1 + " has won!");
+        }
 //player 2 plays
-        //System.out.println("Give " + p2 + "'s next move:");
-        //moveIt(move2);
-        //printBoard();
-        //Winner1(win1, A1, A2, A3, B1, B2, B3, C1, C2, C3);
-
-
-        
+        System.out.println("Give " + p2 + "'s next move:");
+        move2 = input.nextLine();
+        moveIt(move2);
+        printBoard();
+        //Winner1(win1, A1, A2, A3, B1, B2, B3, C1, C2, C3);  
     }
 //reset game
-    System.out.println("Do you want to play again? (Y/N)");
-    playAgain.equals(input.nextLine()); 
+    //System.out.println("Do you want to play again? (Y/N)");
+    //playAgain = input.nextBoolean(); 
   }
 //print out updated board; to be called in main code
   public static void printBoard()
@@ -63,130 +68,129 @@ public class App {
       //move1.equals(move1);
         if (move1.equals("A1"))
         {
-            A1.equals("x");
+            A1 = "x";
         }
         else if (move1.equals("A2"))
         {
-            A2.equals("x");
+            A2 = "x";
         }
         else if (move1.equals("A3"))
         {
-            A3.equals("x");
+            A3 = "x";
         }
         else if (move1.equals("B1"))
         {
-            B1.equals("x");
+            B1 = "x";
         }
         else if (move1.equals("B2"))
         {
-            B2.equals("x");
+            B2 = "x";
         }
         else if (move1.equals("B3"))
         {
-            B3.equals("x");
+            B3 = "x";
         }
         else if (move1.equals("C1"))
         {
-            C1.equals("x");
+            C1 = "x";
         }
         else if (move1.equals("C2"))
         {
-            C2.equals("x");
+            C2 = "x";
         }
         else if (move1.equals("C3"))
         {
-            C3.equals("x");
+            C3 = "x";
         }
-      
-//function for player 2's move
+
     }
+    //function for Player 2's move
     static public void moveIt (String move2)
     {
-      move2.equals(move2);
+     // move2.equals(move2);
         if (move2.equals("A1"))
         {
-            A1.equals("O");
+            A1 = "o";
         }
         else if (move2.equals("A2"))
         {
-            A2.equals("O");
+            A2 = "o";
         }
         else if (move2.equals("A3"))
         {
-            A3.equals("O");
+            A3 = "o";
         }
         else if (move2.equals("B1"))
         {
-            B1.equals("O");
+            B1 = "o";
         }
         else if (move2.equals("B2"))
         {
-            B2.equals("O");
+            B2 = "o";
         }
         else if (move2.equals("B3"))
         {
-            B3.equals("O");
+            B3 = "o";
         }
         else if (move2.equals("C1"))
         {
-            C1.equals("O");
+            C1 = "o";
         }
         else if (move2.equals("C2"))
         {
-            C2.equals("O");
+            C2 = "o";
         }
         else 
         {
-            C3.equals("O");
+            C3 = "o";
         }
       
     }
 //determines whether someone has won
   static public void Winner1 (boolean win1, String A1, String A2, String A3, String B1, String B2, String B3,  String C1, String C2, String C3)
   {
-    win1 = win1;
-    A1.equals(A1);
-    A2.equals(A2);
-    A3.equals(A3);
-    B1.equals(B1);
-    B2.equals(B2);
-    B3.equals(B3);
-    C1.equals(C1);
-    C2.equals(C2);
-    C3.equals(C3);
-    // Check if first column has won
-    //if ((A1.equals("X")) //&& 
-     //   (A2 != "_" || A2 !="O") //&& 
-      //  (A3 != "_" || "O")
-        
+  win1 = win1;
+  A1 = A1;
+  A2 = A2;
+  A3 = A3;
+  B1 = B1;
+  B2 = B2;
+  B3 = B3;
+  C1 = C1;
+  C2 = C2;
+  C3 = C3;
+    //check player 1 has won with any columns
+        if ((A1.equals("X")) && (A2.equals("X")) && (A3.equals("X")))
         {
           win1 = true;
         }
-        if (B1 && B2 && B3 != "_" || "O")
+        if ((B1.equals("X")) && (B2.equals("X")) && (B3.equals("X")))
         {
           win1 = true;
         }
-        else if (C1 && C2 && C3 != "_" || "O")
+        else if ((C1.equals("X")) && (C2.equals("X")) && (C3.equals("X")))
         {
           win1 = true;
         }
-        else if (A1 && B1 && C1 != "_" || "O")
+    //check if player 1 has won with any rows
+        else if ((A1.equals("X")) && (B1.equals("X")) && (C1.equals("X")))
         {
           win1 = true;
         }
-        else if (A2 && B2 && C2 != "_" || "O")
+        else if ((A2.equals("X")) && (B2.equals("X")) && (C2.equals("X")))
         {
           win1 = true;
         }
-        else if (A3 && B3 && C3 != "_" || "O")
+        else if ((C1.equals("X")) && (C2.equals("X")) && (C3.equals("X")))
         {
           win1 = true;
         }
-        else if (A1 && B2 && C3 != "_" || "O")
+    //check is player 1 has won with any rows
+        else if ((A1.equals("X")) && (B2.equals("X")) && (C3.equals("X")))
         {
           win1 = true;
         }
-        else if (A3 && B2 && C != "_" || "O")
+        else if ((A3.equals("X")) && (B2.equals("X")) && (C1.equals("X")))
         {
           win1 = true;
         }
